39b27ba673fecf7ec40b7e3717f1765a23df4918
LeoMao <LMao20001@gmail.com>
2015-05-31 23:50:03 +0800

    add pid first layer (may has bugs...)

--- simulator/main.py
+++ simulator/main.py
@@ -7,38 +7,59 @@ from num_model import Drone
 
 np.set_printoptions(precision=4, suppress=True)
 
-def simulate(drone):
+def get_pid1():
     KPxy = 2
+    KPz = 2
+    KPt = 1
+    kp = np.vstack([np.diag([KPxy, KPxy, KPz]), np.zeros((3, 3))])
+    kd = np.array([1.0]*3 + [1.2]*3)[np.newaxis].T * kp
+    ki = -0.1 * kp
+    ke = 0.9
+    controller = PIDController(kp, kd, ki, ke)
+    return controller
+
+def get_pid2():
+    KPxy = 15
     KPz = 15
     KPw = 5
-    kpw1 = [-KPxy, -KPxy, KPz, KPw, -KPw, KPw]
-    kpw2 = [-KPxy, KPxy, KPz, -KPw, -KPw, -KPw]
-    kpw3 = [KPxy, KPxy, KPz, -KPw, KPw, KPw]
-    kpw4 = [KPxy, -KPxy, KPz, KPw, KPw, -KPw]
+    kpw1 = [KPxy, KPxy, KPz, KPw, -KPw, KPw]
+    kpw2 = [KPxy, -KPxy, KPz, -KPw, -KPw, -KPw]
+    kpw3 = [-KPxy, -KPxy, KPz, -KPw, KPw, KPw]
+    kpw4 = [-KPxy, KPxy, KPz, KPw, KPw, -KPw]
     kp = np.array([kpw1, kpw2, kpw3, kpw4])
     kd = np.array([1/3]*3 + [1.2]*3) * kp
     ki = -0.1 * kp
     ke = 0.9
     controller = PIDController(kp, kd, ki, ke)
+    return controller
+
+def simulate(drone):
     last_time = 0
     G = drone.g
-    DES = np.array([0, 0, G, 0, 0, 0])
+    ctl1 = get_pid1()
+    ctl2 = get_pid2()
+    DES = np.array([0, 0, 0])
     action = np.array([0.0] * 4)
     while True:
         drone.step()
         if drone.get_time() - last_time > 5e-3:
             dt = drone.get_time() - last_time
             last_time = drone.get_time()
+
+            pos = drone.get_position()
             meas = np.array(drone.get_sensors()).flatten()
-            u = controller.get_control(last_time, dt, meas, DES)
+            uacc = ctl1.get_control(last_time, dt, pos, DES)
+            uacc[2] += G
+            u = ctl2.get_control(last_time, dt, meas, uacc)
             action += u
             drone.set_motors(action)
 
-            pos = drone.get_position()
             ori = drone.get_orientation()
             print('update motors: {}'.format(u))
             print('action: {}'.format(action))
+            print('time: {}'.format(last_time))
             print('pos : {}'.format(pos))
+            print('vel : {}'.format(drone.vel))
             print('ori : {}'.format(ori))
             print('meas : {}'.format(meas))
         # pos = drone.get_position()
@@ -46,7 +67,9 @@ def simulate(drone):
 
 def main():
     drone = Drone()
-    drone.set_init([0, 0, 0], [1, 0, 0])
+    drone.set_init([0, 0, 0], [2, 2, 0])
+    # drone.noise_acc = 1e-10
+    # drone.noise_omega = 1e-10
     try:
         simulate(drone)
     except KeyboardInterrupt:
