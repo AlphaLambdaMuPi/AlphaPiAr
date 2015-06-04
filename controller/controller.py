fix rpi/main.py, tweak rotation test parameters

controller/controller.py
@@ -14,7 +14,7 @@ class Controller:
         self.loop = loop if loop else asyncio.get_event_loop()
         self.drone = drone
         self.action = np.array([0., 0., 0., 0.])
-        self.action[1] = self.action[3] = 1250
+        self.action[1] = self.action[3] = 50
         self.despos = np.array([0., 0., 0.])
 
         self.stop_signal = False
@@ -117,7 +117,7 @@ class Controller:
         u = self.ctl2.get_control(now, dt, meas, uacc)
         self.action += u
         self.action = np.maximum.reduce([self.action, np.zeros(4)])
-        self.action = np.minimum.reduce([self.action, np.full((4,), 800)])
+        self.action = np.minimum.reduce([self.action, np.full((4,), 100)])
         yield from self.drone.set_motors(self.action)
 
         self.last_time = now